0d259e6434175b814cb61ee9f9e19a270b06a5b2
VA2002 <122964882+VA2002@users.noreply.github.com>
2024-02-20 08:58:43 +0400

    Add files via upload

---
+++ product.js
@@ -0,0 +1,29 @@
+let products = [
+    {
+        "id": 1001,
+        "title": "Cat Food",
+        "description": "25 lbs bag of <em>irresistible</em>, organic goodness for your cat",
+        "price": 20.00,
+        "img": "https://upload.wikimedia.org/wikipedia/en/b/bc/Garfield_the_Cat.svg",
+        "availableInventory": 10,
+        "rating": 2
+    },
+    {
+        "id": 1002,
+        "title": "Yarn",
+        "description": "Yarn your cat can play with for a very <strong>LONG</strong> time!",
+        "price": 5.50,
+        "img": "https://static.vecteezy.com/system/resources/previews/006/848/242/original/yarn-ball-cartoon-searching-with-happy-gesture-vector.jpg",
+        "availableInventory": 10,
+        "rating": 4
+    },
+    {
+        "id": 1003,
+        "title": "Cat House",
+        "description": "A place for your cat to play",
+        "price": 100.00,
+        "img": "https://cdn-icons-png.flaticon.com/512/3048/3048180.png",
+        "availableInventory": 10,
+        "rating": 4
+    },
+]
