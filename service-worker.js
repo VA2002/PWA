586ac316bd4bba6453b372d79c379a753056a228
VA2002 <122964882+VA2002@users.noreply.github.com>
2024-02-20 10:59:57 +0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -3,13 +3,12 @@ var cacheFiles = [
     "index.html",
     "product.js",
     "petstore.webmanifest",
-    "cat.jpg",
-    "image/yarn.jpg",
+    "images/cat.jpg",
+    "images/yarn.jpg",
     "images/cat-litter.jpg",
     "images/laser-pointer.jpg",
     "images/cat-house.jpg",
     "images/icon-store-512.jpg",
-    "https://upload.wikimedia.org/wikipedia/en/b/bc/Garfield_the_Cat.svg"
 ];
 
 self.addEventListener('install', (e) => {
