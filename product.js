10fc07947b8bd6c226c8f162995469e1d2ed8203
VA2002 <122964882+VA2002@users.noreply.github.com>
2024-02-24 14:38:02 +0400

    Add files via upload

--- product.js
+++ product.js
@@ -1,29 +1,38 @@
 let products = [
     {
-        "id": 1001,
-        "title": "Cat Food",
-        "description": "25 lbs bag of <em>irresistible</em>, organic goodness for your cat",
-        "price": 20.00,
-        "img": "https://upload.wikimedia.org/wikipedia/en/b/bc/Garfield_the_Cat.svg",
-        "availableInventory": 10,
-        "rating": 2
+      "id": 1002,
+      "title": "Yarn",
+      "description": "Yarn your cat can play with for a very <strong>long</strong> time!",
+      "price": 2.99,
+      "image": "images/yarn.jpg",
+      "availableInventory": 7,
+      "rating": 3
     },
     {
-        "id": 1002,
-        "title": "Yarn",
-        "description": "Yarn your cat can play with for a very <strong>LONG</strong> time!",
-        "price": 5.50,
-        "img": "https://static.vecteezy.com/system/resources/previews/006/848/242/original/yarn-ball-cartoon-searching-with-happy-gesture-vector.jpg",
-        "availableInventory": 10,
-        "rating": 4
+      "id": 1003,
+      "title": "Kitty Litter",
+      "description": "Premium kitty litter for your cat.",
+      "price": 11.00,
+      "image": "images/cat-litter.jpg",
+      "availableInventory": 99,
+      "rating": 4
     },
     {
-        "id": 1003,
-        "title": "Cat House",
-        "description": "A place for your cat to play",
-        "price": 100.00,
-        "img": "https://cdn-icons-png.flaticon.com/512/3048/3048180.png",
-        "availableInventory": 10,
-        "rating": 4
+      "id": 1004,
+      "title": "Cat House",
+      "description": "A place for your cat to play!",
+      "price": 7.99,
+      "image": "images/cat-house.jpg",
+      "availableInventory": 11,
+      "rating": 5
     },
-]
+    {
+      "id": 1005,
+      "title": "Laser Pointer",
+      "description": "Drive your cat crazy with this <em>amazing</em> product.",
+      "price": 49.99,
+      "image": "images/laser-pointer.jpg",
+      "availableInventory": 25,
+      "rating": 4
+    }
+  ]
--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,50 @@
 var cacheName = 'petstore-v1';
 var cacheFiles = [
-    "index.html",
-    "product.js",
-    "petstore.webmanifest",
-    "images/cat.jpg",
-    "images/yarn.jpg",
-    "images/cat-litter.jpg",
-    "images/laser-pointer.jpg",
-    "images/cat-house.jpg",
-    "images/icon-store-512.jpg",
+    'index.html',
+    'products.js',
+    'petstore.webmanifest',
+    'images/yarn.jpg',
+    'images/cat-litter.jpg',
+    'images/laser-pointer.jpg',
+    'images/cat-house.jpg',
+    'images/icon-store-512.png'  
 ];
 
 self.addEventListener('install', (e) => {
-    console.log("[Service Worker] Install");
+    console.log('[Service Worker] Install');
     e.waitUntil(
         caches.open(cacheName).then((cache) => {
-            console.log("[Service Worker] Caching all files");
+            console.log('[Service Worker] Caching all the files');
             return cache.addAll(cacheFiles);
         })
     );
 });
 
-// self.addEventListener('fetch', function(e) {
-//     e.respondWith(
-//         caches.match(e.request).then(function(r) {
-//             console.log('[Service Worker] Fetching Resource: ' + e.request.url);
-//             return r;
-//         })
-//     );
-// });
-
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', function (e) {
     e.respondWith(
-        caches.match(e.request).then(function(r) {
-            return r || fetch(e.request).then(function(res) {
-                return caches.open(cacheName).then(function (cache) {
-                    cache.put(e.request, res.clone());
-                    return res;
+        caches.match(e.request).then(function (r) {
+            // Return cached file if it exists
+            if (r) {
+                console.log('[Service Worker] Returning from Cache: ' + e.request.url);
+                return r;
+            }
+
+            // If the file is not in the cache, fetch it from the network
+            return fetch(e.request).then(function (response) {
+                // Check if a valid response was received
+                if (!response || response.status !== 200 || response.type !== 'basic') {
+                    return response;
+                }
+
+                // Clone the response
+                var responseToCache = response.clone();
+
+                // Open the cache and add the new file
+                caches.open(cacheName).then(function (cache) {
+                    cache.put(e.request, responseToCache);
                 });
+
+                return response;
             });
         })
     );
